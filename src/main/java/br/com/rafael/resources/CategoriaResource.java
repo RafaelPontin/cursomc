10e5b29213b3d41175299f05cc518e207eccb1f0
rafael pontin <=>
2019-02-19 22:35:34 -0300

    Categoria repositorio

--- src/main/java/br/com/rafael/resources/CategoriaResource.java
+++ src/main/java/br/com/rafael/resources/CategoriaResource.java
@@ -1,17 +1,30 @@
 package br.com.rafael.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.rafael.domain.Categoria;
+import br.com.rafael.services.CategoriaService;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 
-	@RequestMapping(method=RequestMethod.GET)
-	public String listar()
+	@Autowired
+	private CategoriaService catService;
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id)
 	{
-		return "O REST funciona";
+		Categoria categoria = catService.find(id);
+		return ResponseEntity.ok(categoria);
 	}
 	
 }
---
+++ src/main/java/br/com/rafael/services/CategoriaService.java
@@ -0,0 +1,26 @@
+package br.com.rafael.services;
+
+import java.security.cert.PKIXRevocationChecker.Option;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.rafael.domain.Categoria;
+import br.com.rafael.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	public Categoria find(Integer id)
+	{
+		Optional<Categoria> listCategoria = categoriaRepository.findById(id);
+		return listCategoria.orElse(null);
+	}
+	
+	
+	
+}
